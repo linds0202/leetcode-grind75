bca328613df7bed658f812874b778a46f5df935d
linds0202 <lindsayarainey@gmail.com>
2022-09-19 08:45:45 -0700

    balanced binary tree easy

---
+++ js/balanced-binary-tree.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+// /**
+//  * @param {TreeNode} root
+//  * @return {boolean}
+//  */
+const ckeckHeight = node => {
+    if (node === null) return 0;
+    
+    const left = ckeckHeight(node.left);
+    const right = ckeckHeight(node.right);
+    
+    if (
+      // if a previous call has returned false,
+      // we need to pass false all the way up
+      left === false ||
+      right === false ||
+      Math.abs(left - right) > 1
+    ) {
+      return false;
+    }
+    
+    // height of a node
+    return Math.max(left, right) + 1;
+  };
+  
+  const isBalanced = root => {
+    if (root === null) return true;
+    
+    return ckeckHeight(root) !== false;
+  };

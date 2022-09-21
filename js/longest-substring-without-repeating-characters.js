5f1aa732acb77aae1a6ba40f34e0a915c1383b77
linds0202 <lindsayarainey@gmail.com>
2022-09-21 08:49:13 -0700

    longest substring without repeating characters medium

---
+++ js/longest-substring-without-repeating-characters.js
@@ -0,0 +1,19 @@
+// /**
+//  * @param {string} s
+//  * @return {number}
+//  */
+ var lengthOfLongestSubstring = function(s) {
+    let map = {};
+    let len = s.length;
+    let max = 0;
+    let start = 0;
+    
+    for (let i = 0; i < len; i++) {
+        if (map[s[i]] !== undefined) {
+            start = Math.max(start, map[s[i]] + 1);
+        }
+        map[s[i]] = i;
+        max = Math.max(max, i - start + 1);
+    }
+    return max;
+};

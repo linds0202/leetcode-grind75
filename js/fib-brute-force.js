cb4a011b149732351568c362d8e78db05ad5b006
linds0202 <lindsayarainey@gmail.com>
2022-12-21 13:59:01 -0800

    more fibonacci

--- js/fib-brute-force.js
+++ js/fib-brute-force.js
@@ -1,11 +1,12 @@
 const fib = (n) => {
-    let total = 0
-    for (let i = n; i > 0; i--) {
-        console.log(i)
-        total += i
-    }
+    if (n === 1) return 0
+    if (n=== 2) return 1
 
-    return total
+    let fibs = [0, 1]
+    for (let i = 1; i < n; i++) {
+        fibs.push(fibs[i - 1] + fibs[i])
+    }
+    return fibs.pop()
 }
 
 console.log(fib(4))
